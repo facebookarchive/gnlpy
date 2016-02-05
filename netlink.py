d38b23b2843623e5ee5792029399262046d29bce
chantra <chantr4@gmail.com>
2016-02-04 21:59:34 -0800

    [unittest] Add unittests to test MessageType and NetlinkSocket

--- netlink.py
+++ netlink.py
@@ -52,6 +52,8 @@ So, for this all to work, we must create attribute lists that know how to
 pack and unpack their values, and messages that know what type of attribute
 lists they have.  This is accomplished with create_attr_list_type and
 create_genl_message_type.
+In some cases, some messages may not be supported when called from userland. In
+such case, provide a attribute list of None.
 
 Usage:
 
@@ -71,7 +73,8 @@ Usage:
     Msg = netlink.create_genl_message_type(
         'Msg', 'SPECIFIED_KERNEL_NAME',
         ('COMMAND_1', ListA),
-        ('COMMAND_1', ListB),
+        ('COMMAND_2', None),  # COMMAND_2 is not available via userland.
+        ('COMMAND_3', ListB),
     )
 
 And at this point, you can begin sending and receiving `Msg`es to a netlink
@@ -314,7 +317,8 @@ def create_genl_message_type(class_name, family_id_or_name, *fields,
             self.flags = flags
 
             if attr_list is _unset:
-                self.attr_list = key_to_attr_list_type[self.cmd]()
+                kls = key_to_attr_list_type[self.cmd]
+                self.attr_list = kls is not None and kls() or kls
             else:
                 self.attr_list = attr_list
 
@@ -350,10 +354,10 @@ __to_lookup_on_init = set()
 
 
 def message_class(msg_class):
-    if msg_class.family in __cmd_unpack_map:
-        return
-    if msg_class in __to_lookup_on_init:
-        return
+    assert msg_class.family not in __cmd_unpack_map, \
+        'Message class %d is already defined' % msg_class.family
+    assert msg_class.family not in [x.family for x in __to_lookup_on_init], \
+        'Message class %s is already defined' % msg_class.family
 
     if not isinstance(msg_class.family, int):
         __to_lookup_on_init.add(msg_class)
@@ -422,6 +426,7 @@ CtrlAttrList = create_attr_list_type(
 CtrlMessage = create_genl_message_type(
     'CtrlMessage',
     16,
+    # NEWFAMILY message is returned in response to a GETFAMILY request.
     ('NEWFAMILY', CtrlAttrList),
     ('DELFAMILY', None),
     ('GETFAMILY', CtrlAttrList),
@@ -542,6 +547,7 @@ class NetlinkSocket(object):
     def execute(self, request):
         with self.lock:
             try:
+                messages = None
                 self._send(request)
                 messages = self._recv()
                 assert len(messages) == 1
--- tests/netlink_tests.py
+++ tests/netlink_tests.py
@@ -7,6 +7,8 @@ from __future__ import unicode_literals
 
 from gnlpy import netlink
 
+import mock
+import socket
 import unittest
 
 
@@ -20,23 +22,22 @@ def pack_unpack(kls, s):
 
 class AttrListTestCase(unittest.TestCase):
     '''
-    Test AttrListType class
+    Test AttrListType class.
     '''
-    def setUp(self):
-        self.AttrListTest = netlink.create_attr_list_type(
-            'AttrListTest',
-            ('U8TYPE', netlink.U8Type),
-            ('U16TYPE', netlink.U16Type),
-            ('U32TYPE', netlink.U32Type),
-            ('U64TYPE', netlink.U64Type),
-            ('I32TYPE', netlink.I32Type),
-            ('NET16TYPE', netlink.Net16Type),
-            ('NET32TYPE', netlink.Net32Type),
-            ('IGNORETYPE', netlink.IgnoreType),
-            ('BINARYTYPE', netlink.BinaryType),
-            ('NULSTRINGTYPE', netlink.NulStringType),
-            ('RECURSIVESELF', netlink.RecursiveSelf),
-        )
+    AttrListTest = netlink.create_attr_list_type(
+        'AttrListTest',
+        ('U8TYPE', netlink.U8Type),
+        ('U16TYPE', netlink.U16Type),
+        ('U32TYPE', netlink.U32Type),
+        ('U64TYPE', netlink.U64Type),
+        ('I32TYPE', netlink.I32Type),
+        ('NET16TYPE', netlink.Net16Type),
+        ('NET32TYPE', netlink.Net32Type),
+        ('IGNORETYPE', netlink.IgnoreType),
+        ('BINARYTYPE', netlink.BinaryType),
+        ('NULSTRINGTYPE', netlink.NulStringType),
+        ('RECURSIVESELF', netlink.RecursiveSelf),
+    )
 
     def test_getter_no_default(self):
         a = self.AttrListTest()
@@ -49,10 +50,13 @@ class AttrListTestCase(unittest.TestCase):
     def test_packing(self):
         a = self.AttrListTest(
             u64type=2,
+            binarytype='ABCD',
             nulstringtype='abcd',
         )
 
         b = pack_unpack(self.AttrListTest, a)
+        self.assertEqual(b.get('u64type'), 2)
+        self.assertEqual(b.get('binarytype'), 'ABCD')
         self.assertEqual(b.get('nulstringtype'), 'abcd')
 
     def test_recursive_self(self):
@@ -67,3 +71,126 @@ class AttrListTestCase(unittest.TestCase):
         # Confirmd that we can properly pack and unpack the AttrListType.
         b = pack_unpack(self.AttrListTest, a)
         self.assertEqual(b.get('recursiveself').get('nulstringtype'), 'abcd')
+
+
+class MessageTypeTestCase(unittest.TestCase):
+    '''
+    Test MessageType class.
+    '''
+    AttrListTest = netlink.create_attr_list_type(
+        'AttrListTest',
+        ('U8TYPE', netlink.U8Type),
+        ('U16TYPE', netlink.U16Type),
+        ('U32TYPE', netlink.U32Type),
+        ('U64TYPE', netlink.U64Type),
+        ('I32TYPE', netlink.I32Type),
+        ('NET16TYPE', netlink.Net16Type),
+        ('NET32TYPE', netlink.Net32Type),
+        ('IGNORETYPE', netlink.IgnoreType),
+        ('BINARYTYPE', netlink.BinaryType),
+        ('NULSTRINGTYPE', netlink.NulStringType),
+        ('RECURSIVESELF', netlink.RecursiveSelf),
+    )
+
+    CtrlMessageTest = netlink.create_genl_message_type(
+        'CtrlMessageTest',
+        12345,
+        ('CMD1', AttrListTest),
+        ('CMD2', None),
+    )
+
+    def test_msg_type_init(self):
+        attr = self.AttrListTest()
+        # Raises a KeyError when the command is not supported by the message.
+        with self.assertRaises(KeyError):
+            self.CtrlMessageTest('CMD', attr_list=attr)
+
+        # A message can be initialized with a CMD string
+        ctrl = self.CtrlMessageTest('CMD1', attr_list=attr)
+        self.assertEqual(ctrl.cmd, 1)
+
+        # Or a command ID.
+        ctrl2 = self.CtrlMessageTest(1, attr_list=attr)
+        self.assertEqual(ctrl.cmd, ctrl2.cmd)
+
+        # When initialized without an attribute list, it will create a default
+        # attr_list for us.
+        ctrl = self.CtrlMessageTest('CMD1')
+        self.assertIsInstance(ctrl.attr_list, self.AttrListTest)
+        # Unless the operation is not supported
+        ctrl = self.CtrlMessageTest('CMD2')
+        self.assertIsNone(ctrl.attr_list)
+
+    def test_get_attr_list(self):
+        ctrl = self.CtrlMessageTest('CMD1')
+        self.assertIsInstance(ctrl.get_attr_list(), self.AttrListTest)
+
+    def test_assert_on_message_redefinition(self):
+        # Assert when redefining a MessageType
+        with self.assertRaises(AssertionError):
+            netlink.create_genl_message_type(
+                'Foo',
+                12345,
+            )
+        # Ok to create new MessageType ID.
+        # class_name is only used in repr, it does not technically need to be
+        # unique.
+        netlink.create_genl_message_type('Foo', 12346)
+
+    def test_new_message_by_name(self):
+        # It is fine to create a message by using a family name.
+        # It will be looked up later.
+        netlink.create_genl_message_type('Foo', 'BAR')
+        # But we should not be able to register twice!
+        with self.assertRaises(AssertionError):
+            netlink.create_genl_message_type('Foo', 'BAR')
+
+    def test_packing(self):
+        attr = self.AttrListTest(
+            u64type=2,
+            binarytype='ABCD',
+            nulstringtype='abcd',
+        )
+
+        ctrl = self.CtrlMessageTest('CMD1', attr_list=attr)
+        self.assertEqual(ctrl.cmd, 1)
+        ctrl2 = pack_unpack(self.CtrlMessageTest, ctrl)
+        # Check that we have the same command.
+        self.assertEqual(ctrl.cmd, ctrl2.cmd)
+        # And that we have the same attributes.
+        self.assertEqual(
+            ctrl.attr_list.get('binarytype'),
+            ctrl2.attr_list.get('binarytype')
+        )
+
+
+class NetlinkSocketTestCase(unittest.TestCase):
+
+    def test_asocket_open_close(self):
+        with mock.patch.object(
+                netlink.NetlinkSocket,
+                'resolve_family',
+                return_value=5) as mock_method:
+            sock = netlink.NetlinkSocket()
+            sock.close()
+        mock_method.assert_called_once_with('BAR')
+
+    def test_msg_query_exception_verbose(self):
+        self.sock = mock.MagicMock(name='netlink.socket', spec=socket.socket)
+        self.sock.recv.return_value = 'AAA'
+        sock = netlink.NetlinkSocket(verbose=True)
+        with self.assertRaises(AttributeError):
+            sock.query('a')
+        sock.close()
+
+    def test_msg_execute_exception_verbose(self):
+        self.sock = mock.MagicMock(name='netlink.socket', spec=socket.socket)
+        self.sock.recv.return_value = 'AAA'
+        sock = netlink.NetlinkSocket(verbose=True)
+        with self.assertRaises(AttributeError):
+            sock.execute('a')
+        sock.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
